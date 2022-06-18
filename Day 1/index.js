0d78ec88efdcd491e93082ead14fa98191efc694
sheriff-gaye <sheriffgaye5@gmail.com>
2022-06-18 23:52:35 +0000

    js

--- Day 1/index.js
+++ Day 1/index.js
@@ -3,3 +3,22 @@ function add(x,y)
 {
     return x+y;
 }
+add(3,3) //it returns 6
+
+function mul(x,y)
+{
+    return x*y;
+}
+mul(9,3) //it return 27
+
+function sub(x,y)
+{
+    return x-y
+}
+sub(8,2) //it return 6
+
+function div(x,y)
+{
+    return x/y;
+}
+div(10,5) // it return 2
---
+++ Day 3/index.js
@@ -0,0 +1,6 @@
+//javascript array
+
+new arr=['sheriff','gaye']
+
+//another way of creating array in javascript
+var arr=new Array("fatou",'lamin','modou')
