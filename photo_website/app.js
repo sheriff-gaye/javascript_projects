d44f72dfcc59f485a80ad61863108262e68cea8f
sheriff-gaye <sheriffgaye5@gmail.com>
2022-08-31 20:51:09 +0000

    hello

--- photo_website/app.js
+++
@@ -1,9 +0,0 @@
-
-
-//nav scroll color change
-
-window.addEventListener('scroll',()=>{
-    document.querySelector('nav').classList.toggle('window-scrolled'),window.screenY>0;
-})
-
-//end of nav scroll
