3aaf552ace8dfb685d348ee5e61db5970a208869
sheriff-gaye <sheriffgaye5@gmail.com>
2022-09-02 22:52:48 +0000

    hello

---
+++ converter/app.js
@@ -0,0 +1,11 @@
+
+
+const button=document.getElementById('btn')
+const screen_1=document.getElementById('first')
+const screen_2=document.getElementById('second')
+
+
+button.addEventListener('click',()=>{
+    screen2_value=screen_1/100
+    console.log(screen2_value)
+},{once:true});
