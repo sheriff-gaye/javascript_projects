ff0d4773b6f10df4f89a11e29c6a65beb00294ab
sheriff-gaye <sheriffgaye5@gmail.com>
2024-03-06 01:00:56 +0000

    clean code

--- school/app.js
+++ school/app.js
@@ -1,28 +1,48 @@
 
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const targetElement = document.getElementById('typewriter-container');
+
+    const typewriter = new Typewriter(targetElement, {
+        strings: [
+            `Elevate your <span style="color: #c50c37;">skills</span>, advance`,
+            `Skills for <span style="color: #c50c37;">career progression</span>`,
+            `Forge ahead with <span style="color: #c50c37;">new skills</span>`,
+            `Advance through <span style="color: #c50c37;">learned skills</span>`,
+            `<span style="color: #c50c37;">Skills that propel careers</span>`,
+        ]
+        ,
+        autoStart: true,
+        loop: true,
+    });
+});
+
+
 // change nav bar style on scroll
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
 
-    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>0)
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
 
 //hide and show faqs answer
 
-const faqs=document.querySelectorAll('.faq');
+const faqs = document.querySelectorAll('.faq');
 
-faqs.forEach(faq=>{
-    faq.addEventListener('click',()=>{
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
         faq.classList.toggle('open');
 
         //changing the plus icon
 
-        const icon =faq.querySelector('.faq_icon i')
+        const icon = faq.querySelector('.faq_icon i')
 
-        if (icon.className==='uil uil-plus'){
-            icon.className='uil uil-minus'
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus'
         }
-        else{
-            icon.className='uil uil-plus'
+        else {
+            icon.className = 'uil uil-plus'
         }
     })
 })
@@ -31,21 +51,21 @@ faqs.forEach(faq=>{
 
 //nav bar  media query open and close
 
-const menu=document.querySelector('.nav_menu')
-const openBtn=document.querySelector('#open-menu-btn')
-const closeBtn=document.querySelector('#close-menu-btn')
+const menu = document.querySelector('.nav_menu')
+const openBtn = document.querySelector('#open-menu-btn')
+const closeBtn = document.querySelector('#close-menu-btn')
 
 
-openBtn.addEventListener('click',()=>{
-    menu.style.display="flex";
-    openBtn.style.display="none";
-    closeBtn.style.display="inline-block";
+openBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    openBtn.style.display = "none";
+    closeBtn.style.display = "inline-block";
 
 })
 
-closeBtn.addEventListener('click',()=>{
-    menu.style.display="none";
-    openBtn.style.display="inline-block"
-    closeBtn.style.display="none"
+closeBtn.addEventListener('click', () => {
+    menu.style.display = "none";
+    openBtn.style.display = "inline-block"
+    closeBtn.style.display = "none"
 
 })
